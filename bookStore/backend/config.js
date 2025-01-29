2336d8b949b6d41f74ee5218bdbfa8dbab564c5e
javier.emanuel.hr <164814665+javieremanuelhr@users.noreply.github.com>
2025-01-29 04:04:28 -0600

    Delete bookStore directory

--- bookStore/backend/config.js
+++
@@ -1,3 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDBURL = 'mongodb+srv://javieremanuelhr:Javieremanuel2@bookstoremern.hpvwq.mongodb.net/bookStoreMERN?retryWrites=true&w=majority&appName=bookStoreMERN';
--- bookStore/frontend/bookStoreFrontend/src/pages/deleteBook.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const deleteBook = () => {
-  return (
-    <div>deleteBook</div>
-  )
-}
-
-export default deleteBook
