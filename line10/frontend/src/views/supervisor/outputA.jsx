a0557c87d7385d64be590287199e87120b0bcb02
javieremanuelhr <javier.emanuel.hr@gamil.com>
2025-02-06 23:47:54 -0600

    throughput per shift going on Feb 6th, 2025

--- line10/frontend/src/views/supervisor/outputA.jsx
+++ line10/frontend/src/views/supervisor/outputA.jsx
@@ -3,27 +3,44 @@ import '../../index.css'
 
 const OutputA = () => {
   return (
-    <div className='w-full h-full font-semibold text text-center border flex flex-row'>
-        <table className='bg-lime-500 w-1/8'>
+    <div className='w-1/3 h-full font-semibold text text-center border flex flex-col'>
+        <table className='bg-lime-500 w-full'>
             <thead>
-                <tr className='bg-black'><th>Throuhput</th></tr>
+                <tr className='bg-black'>
+                    <th>Throuhput</th>
+                    <th>Qty</th>
+                </tr>
             </thead>
-            <tbody className='w-full'>
-                <tr><td>Main</td></tr>
-                <tr><td><input></input></td></tr>
-                <tr><td>Coex</td></tr>
-                <tr><td><input></input></td></tr>
-                <tr><td>Total</td></tr>
-                <tr><td><label></label></td></tr>
+            <tbody>
+                <tr>
+                    <td>Main</td>
+                    <td className='w-1/2'><input className='w-full'></input></td>
+                </tr>
+                <tr>
+                    <td>Coex</td>
+                    <td className='w-1/2'><input className='w-full'></input></td>
+                </tr>
+                <tr>
+                    <td>Total</td>
+                    <td className='w-1/2'><input className='w-full'></input></td>
+                </tr>
             </tbody>
         </table>
         <table className='w-full'>
             <thead>
-
+                <tr>
+                    <th>MAIN</th>
+                    <th>A</th>
+                    <th>B</th>
+                    <th>C</th>
+                </tr>
             </thead>
             <tbody>
                 <tr>
-                    
+                    <td>RGG</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
                 </tr>
             </tbody>
         </table>
